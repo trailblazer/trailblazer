49b6bf76aa98e4a8e64c82161bec0d52090e82b0
Nick Sutterer <apotonick@gmail.com>
2014-10-31 12:27:27 +1100

    add crud to autoloading.

--- lib/trailblazer/autoloading.rb
+++ lib/trailblazer/autoloading.rb
@@ -1,4 +1,5 @@
 Trailblazer::Operation.class_eval do
   autoload :Controller, 'trailblazer/operation/controller'
-  autoload :Responder, 'trailblazer/operation/responder'
+  autoload :Responder,  'trailblazer/operation/responder'
+  autoload :CRUD,       'trailblazer/operation/crud'
 end
