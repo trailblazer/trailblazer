e308d6cf0147b3870f10073fef4895ad08a47dfd
Nick Sutterer <apotonick@gmail.com>
2014-11-03 21:13:33 +1100

    use CRUD::model_class, not model_name.

--- lib/trailblazer/operation/controller.rb
+++ lib/trailblazer/operation/controller.rb
@@ -63,7 +63,7 @@ private
     unless request.format == :html
       # this is what happens:
       # respond_with Comment::Update::JSON.run(params.merge(comment: request.body.string))
-      concept_name = operation_class.model_name # this could be renamed to ::concept_name soon.
+      concept_name = operation_class.model_class # this could be renamed to ::concept_class soon.
       params.merge!(concept_name => request.body.string)
     end
 
