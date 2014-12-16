06805ab090b21ec0898f5569ec92b5edb26d7dfc
Nick Sutterer <apotonick@gmail.com>
2014-12-17 09:58:28 +1100

    add crud_autoloading which goes through app/concepts, finds all crud.rb files and then autoloads the model and then the actual crud file.

--- lib/trailblazer/crud_autoloading.rb
+++ lib/trailblazer/crud_autoloading.rb
@@ -1,1 +1,8 @@
-Dir.glob("app/concepts/**/crud.rb") { |f| require_dependency f, true }
+# FIXME: this will only work for operations embedded in a MODEL namespace.
+# this is really hacky and assumes a lot about your structure, but it works for now. don't include it if you don't like it.
+Dir.glob("app/concepts/**/crud.rb") do |f|
+  path = f.sub("app/concepts/", "")
+
+  path.sub("/crud.rb", "").camelize.constantize # load the model first (Thing).
+  require_dependency path # load model/crud.rb (Thing::Create, Thing::Update, and so on).
+end
