17b48ecd2e50e4eac0220ff534dc47f4d0237245
Nick Sutterer <apotonick@gmail.com>
2015-01-11 20:11:55 +1100

    cleanup.

--- lib/trailblazer/crud_autoloading.rb
+++ lib/trailblazer/crud_autoloading.rb
@@ -1,10 +1,7 @@
 # FIXME: this will only work for operations embedded in a MODEL namespace.
 # this is really hacky and assumes a lot about your structure, but it works for now. don't include it if you don't like it.
 Dir.glob("app/concepts/**/crud.rb") do |f|
-  path = f.sub("app/concepts/", "")
-
-
-
+  path  = f.sub("app/concepts/", "")
   model = path.sub("/crud.rb", "")
 
   require_dependency "app/models/#{model}" # load the model file, first (thing.rb).
