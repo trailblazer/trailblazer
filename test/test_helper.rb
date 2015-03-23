d8379d361c385e84871541bbe281485c7c7d68fd
Celso Fernandes <fernandes@zertico.com>
2015-03-23 13:37:20 -0300

    Fix filesystem test on travis

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,11 @@ require 'minitest/autorun'
 
 module TmpUploads
   extend ActiveSupport::Concern
+  fs_dir = File.join(File.dirname(__FILE__), '..', 'tmp', 'uploads')
+  FileUtils.mkdir_p(fs_dir) unless File.exist?(fs_dir)
 
   included do
-    let (:tmp_dir) { "/tmp/uploads" }
-    before { Dir.mkdir(tmp_dir) unless File.exists?(tmp_dir) }
+    let(:tmp_dir) { File.expand_path(fs_dir) }
   end
 end
 
