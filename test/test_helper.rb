768f2b368eed86f71cb31bbfbc8744692f49c5c5
Celso Fernandes <fernandes@zertico.com>
2015-03-23 11:44:21 -0300

    Add ActiveRecord support on tests
    * To make Kaminari and Ransack, we *need* ActiveRecord relations
    * Add database_cleaner to keep database clean state during tests
    * sqlite3 in memory based tests to avoid big dependencies during dev

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,24 @@
 require 'trailblazer'
 require 'minitest/autorun'
+require 'active_record'
+require 'database_cleaner'
+
+# ActiveRecord Connection
+# ActiveRecord::Base.logger = Logger.new(STDERR)
+ActiveRecord::Base.logger = false
+ActiveRecord::Base.establish_connection(
+  adapter:  "sqlite3",
+  database: ":memory:"
+)
+
+# Dot not show Active Record Migrations
+ActiveRecord::Migration.verbose = false
+ActiveRecord::Schema.define do
+  create_table :things do |table|
+    table.column :title, :string
+    table.column :active, :boolean, default: true
+  end
+end
 
 module TmpUploads
   extend ActiveSupport::Concern
@@ -12,4 +31,18 @@ end
 
 MiniTest::Spec.class_eval do
   include TmpUploads
+end
+
+# Database Cleaning
+DatabaseCleaner.strategy = :transaction
+DatabaseCleaner.clean_with(:truncation)
+
+class MiniTest::Spec
+  before :each do
+    DatabaseCleaner.start
+  end
+
+  after :each do
+    DatabaseCleaner.clean
+  end
 end
--- trailblazer.gemspec
+++ trailblazer.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "reform", "~> 1.2.0"
 
   spec.add_development_dependency "bundler", "~> 1.3"
+  spec.add_development_dependency "database_cleaner"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "minitest"
   # spec.add_development_dependency "minitest-spec-rails" # TODO: can anyone make this work (test/rails).
