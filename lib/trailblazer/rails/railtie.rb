1c909b7b3ea87aa28f23aeae813b6efd2b06e7c2
Nick Sutterer <apotonick@gmail.com>
2015-06-07 17:48:48 +1000

    TRY to fix autoloading by only using Rails' reloader. fixes #56 (hopefully).

--- lib/trailblazer/rails/railtie.rb
+++ lib/trailblazer/rails/railtie.rb
@@ -20,14 +20,9 @@ module Trailblazer
     initializer 'trailblazer.install', after: :load_config_initializers do |app|
       # the trb autoloading has to be run after initializers have been loaded, so we can tweak inclusion of features in
       # initializers.
-      if Rails.configuration.cache_classes
+      ActionDispatch::Reloader.to_prepare do
         Trailblazer::Railtie.autoload_operations(app)
         Trailblazer::Railtie.autoload_cells(app)
-      else
-        ActionDispatch::Reloader.to_prepare do
-          Trailblazer::Railtie.autoload_operations(app)
-          Trailblazer::Railtie.autoload_cells(app)
-        end
       end
     end
   end
