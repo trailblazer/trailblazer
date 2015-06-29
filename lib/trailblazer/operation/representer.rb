2202a69fe5991f581fca60b5087987cfdb2b6926
Nick Sutterer <apotonick@gmail.com>
2015-06-30 09:20:03 +1000

    no need for deserializer_class. the chance that people want a separate representer and deserializer are minimal.

--- lib/trailblazer/operation/representer.rb
+++ lib/trailblazer/operation/representer.rb
@@ -1,4 +1,5 @@
 # Including this will change the way deserialization in #validate works.
+#
 # Instead of treating params as a hash and letting the form object deserialize it,
 # a representer will be infered from the contract. This representer is then passed as
 # deserializer into Form#validate.
@@ -22,7 +23,6 @@ module Trailblazer::Operation::Representer
       self._representer_class = constant
     end
 
-
     def infer_representer_class
       Disposable::Twin::Schema.from(contract_class,
         include: [Representable::JSON],
@@ -31,16 +31,12 @@ module Trailblazer::Operation::Representer
         representer_from: lambda { |inline| inline.representer_class },
       )
     end
-
-    def deserializer_class
-      representer_class
-    end
   end
 
   def validate_contract(params)
     # use the inferred representer from the contract for deserialization in #validate.
     contract.validate(params) do |json|
-      self.class.deserializer_class.new(contract).from_json(json)
+      self.class.representer_class.new(contract).from_json(json)
     end
   end
 end
