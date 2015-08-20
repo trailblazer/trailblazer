246bbc0d10e6d296adcc9576d07874df2cef7a19
Nick Sutterer <apotonick@gmail.com>
2015-08-20 16:15:23 +1000

    make pundit work with trailblazer.

--- lib/trailblazer/operation/policy.rb
+++ lib/trailblazer/operation/policy.rb
@@ -3,10 +3,10 @@ module Trailblazer
   end
 
   module Operation::Policy
-    def self.included(base)
-      # base.extend Uber::InheritableAttr
-      # base.inheritable_attr :_representer_class
-      base.extend ClassMethods
+    def self.included(includer)
+      # includer.extend Uber::InheritableAttr
+      # includer.inheritable_attr :_representer_class
+      includer.extend ClassMethods
     end
 
     module ClassMethods
@@ -17,11 +17,42 @@ module Trailblazer
       attr_reader :policies
     end
 
+
     def setup!(params)
       super
       instance_exec params, &self.class.policies.first or raise NotAuthorizedError
       #
       # NotAuthorizedError.new(query: query, record: record, policy: policy)
+    end
+
+
+
+
+
+    require "pundit"
+    module Pundit
+      def self.included(includer)
+        includer.extend ClassMethods
+      end
+
+      module ClassMethods
+        def policy(policy_class, action, &block)
+          @pundit_config = [policy_class, action]
+          #@policies = [block]
+        end
+
+        attr_reader :pundit_config
+      end
+
+      def setup!(params)
+        super
+        # instance_exec params, &self.class.policies.first or raise NotAuthorizedError
+        class_name, action = self.class.pundit_config
+        policy = class_name.new(params[:current_user], model)
+
+        # DISCUSS: this flow should be used via pundit's API, which we might have to extend.
+        policy.send(action) or raise ::Pundit::NotAuthorizedError.new(query: action, record: model, policy: policy)
+      end
     end
   end
 end
--- test/operation/policy_test.rb
+++ test/operation/policy_test.rb
@@ -32,3 +32,46 @@ class OpPolicyTest < MiniTest::Spec
     end
   end
 end
+
+
+
+class OpPunditPolicyTest < MiniTest::Spec
+  Song = Struct.new(:name)
+  User = Struct.new(:name)
+
+  class BlaPolicy
+    def initialize(user, song)
+      @user = user
+      @song = song
+    end
+
+    def create?
+      @user.is_a?(User) and @song.is_a?(Song)
+    end
+  end
+
+  class BlaOperation < Trailblazer::Operation
+    include Policy::Pundit
+    policy BlaPolicy, :create?
+
+    def model!(*)
+      Song.new
+    end
+
+    def process(*)
+    end
+  end
+
+  # valid.
+  it do
+    op = BlaOperation.({current_user: User.new})
+
+  end
+
+  # invalid.
+  it do
+    assert_raises Pundit::NotAuthorizedError do
+      op = BlaOperation.({current_user: nil})
+    end
+  end
+end
