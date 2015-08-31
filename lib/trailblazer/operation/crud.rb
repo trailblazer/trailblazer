d179efd0e2f233aa6e06dd047d1dee2456bda65a
Nick Sutterer <apotonick@gmail.com>
2015-08-31 11:13:07 +1000

    extract model building logic into ModelBuilder so we can use it on the class level, too.

--- lib/trailblazer/operation/crud.rb
+++ lib/trailblazer/operation/crud.rb
@@ -36,29 +36,40 @@ module Trailblazer
       end
 
 
+      module ModelBuilder
+        def model!(params)
+          instantiate_model(params)
+        end
+
+        def instantiate_model(params)
+          send("#{action_name}_model", params)
+        end
+
+        def create_model(params)
+          model_class.new
+        end
+
+        def update_model(params)
+          model_class.find(params[:id])
+        end
+      end
+
+
       # #validate no longer accepts a model since this module instantiates it for you.
       def validate(params, model=self.model, *args)
         super(params, model, *args)
       end
 
     private
-      def model!(params)
-        instantiate_model(params)
-      end
-
-      def instantiate_model(params)
-        send("#{self.class.action_name}_model", params)
-      end
-
-      def create_model(params)
-        self.class.model_class.new
-      end
-
-      def update_model(params)
-        self.class.model_class.find(params[:id])
-      end
-
+      include ModelBuilder
       alias_method :find_model, :update_model
+
+      def model_class
+        self.class.model_class
+      end
+      def action_name
+        self.class.action_name
+      end
 
 
       # Rails-specific.
