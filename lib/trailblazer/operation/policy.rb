1edd63fcdcb584eff8b8e5b9ceb81d97f5efedc3
Nick Sutterer <apotonick@gmail.com>
2015-09-03 17:47:57 +1000

    the standard Policy is not pundit-style (without actually having to use pundit). the policies where you have a block i now call Policy::Guard.

--- lib/trailblazer/operation/policy.rb
+++ lib/trailblazer/operation/policy.rb
@@ -2,15 +2,20 @@ module Trailblazer
   class NotAuthorizedError < RuntimeError
   end
 
-  # Adds #evaluate_policy to Operation#setup!
+  # Adds #evaluate_policy to #setup!, and ::policy.
   module Operation::Policy
     def self.included(includer)
-      includer.inheritable_attr :policy_config
-      includer.policy_config = Permission.new { true } # return true per default.
-      includer.extend ClassMethods
+      includer.extend DSL
+      includer.extend BuildPolicy # so we can use ::build_policy in builders.
+      includer.send(:include, EvaluatePolicy)
     end
 
-    module ClassMethods
+    module DSL
+      def self.extended(extender)
+        extender.inheritable_attr :policy_config
+        extender.policy_config = Guard::Permission.new { true } # return true per default.
+      end
+
       def policy(*args, &block)
         self.policy_config = permission_class.new(*args, &block)
       end
@@ -20,100 +25,83 @@ module Trailblazer
       end
     end
 
+    attr_reader :policy
+
   private
-    def setup!(params)
-      super
-      evaluate_policy(params)
+    module Setup
+      def setup!(params)
+        super
+        evaluate_policy(params)
+      end
+    end
+    include Setup
+
+    module BuildPolicy
+      def build_policy(model, params, permission=self.policy_config)
+        permission.policy(params[:current_user], model)
+      end
+    end
+    include BuildPolicy
+
+
+    module EvaluatePolicy
+    private
+      def evaluate_policy(params)
+        result, @policy, action = self.class.policy_config.(params[:current_user], model)
+
+        result or raise policy_exception(@policy, action, model)
+      end
+
+      def policy_exception(policy, action, model)
+        NotAuthorizedError.new(query: action, record: model, policy: policy)
+      end
     end
 
-    def evaluate_policy(params)
-      self.class.policy_config.(self, params) or raise NotAuthorizedError.new
-    end
-
-
-    # Encapsulates the operation's policy which is usually called in Op#setup!.
+    # Encapsulate building the Policy object and calling the defined query action.
     class Permission
-      def initialize(*args, &block)
-        @callable, @args = Uber::Options::Value.new(block), args
+      def initialize(policy_class, action)
+        @policy_class, @action = policy_class, action
       end
 
-      def call(context, *args)
-        @callable.(context, *args)
+      def call(user, model)
+        policy = policy(user, model)
+        [policy.send(@action), policy, @action]
+      end
+
+      def policy(user, model)
+        @policy_class.new(user, model)
       end
     end
+  end
 
-
-
-
-
-    require "pundit"
-    module Pundit
-      class Permission
-        def initialize(policy_class, action)
-          @policy_class, @action = policy_class, action
-        end
-
-        def call(user, model)
-          policy = policy(user, model)
-          [policy.send(@action), policy]
-        end
-
-        def policy(user, model)
-          @policy_class.new(user, model)
-        end
-
-      # private
-        def action
-          @action
-        end
-      end
-
+  # Adds #evaluate_policy to Operation#setup!
+  module Operation::Policy
+    module Guard
       def self.included(includer)
-        includer.send(:include, Trailblazer::Operation::Policy)
-        includer.extend ClassMethods
-        includer.extend BuildPolicy # so we can use ::build_policy in builders.
-        includer.send(:include, EvaluatePolicy)
+        includer.extend(Trailblazer::Operation::Policy::DSL)
+        includer.extend(ClassMethods)
+        includer.send(:include, Setup)
       end
 
       module ClassMethods
+        # Use Guard::Permission.
         def permission_class
           Permission
         end
       end
 
-      attr_reader :policy
-
-
-      module BuildPolicy
-        def build_policy(model, params, permission=self.policy_config)
-          permission.policy(params[:current_user], model)
-        end
+      def evaluate_policy(params)
+        self.class.policy_config.(self, params) or raise NotAuthorizedError.new
       end
-      include BuildPolicy
 
-
-      module EvaluatePolicy
-      private
-        def evaluate_policy(params)
-          puts "@@@@@ #{self.class.policy_config.inspect}"
-          result, @policy = self.class.policy_config.(params[:current_user], model)
-          result or raise policy_exception(@policy, self.class.policy_config.action, model)
-          # policy!(model, params, self.class.policy_config)
-
-          # # DISCUSS: this flow should be used via pundit's API, which we might have to extend.
-          # evaluate_policy!(@policy, action, model)
+      # Encapsulates the operation's policy which is usually called in Op#setup!.
+      class Permission
+        def initialize(*args, &block)
+          @callable, @args = Uber::Options::Value.new(block), args
         end
 
-        # def policy!(model, params, policy_class)
-        #   @policy = build_policy(model, params, policy_class)
-        # end
-
-        # def evaluate_policy!(policy, action, model)
-        #   policy.send(action) or raise policy_exception(policy, action, model)
-        # end
-
-        def policy_exception(policy, action, model)
-          ::Pundit::NotAuthorizedError.new(query: action, record: model, policy: policy)
+        def call(context, *args)
+          @callable.(context, *args)
         end
       end
     end
--- test/operation/policy_test.rb
+++ test/operation/policy_test.rb
@@ -2,104 +2,6 @@ require "test_helper"
 require "trailblazer/operation/policy"
 
 
-class OpPolicyTest < MiniTest::Spec
-  Song = Struct.new(:name)
-
-  class Create < Trailblazer::Operation
-    include Policy
-
-    def model!(*)
-      Song.new
-    end
-
-    policy do |params|
-      model.is_a?(Song) and params[:valid]
-    end
-
-    def process(*)
-    end
-  end
-
-  # valid.
-  it "whoo" do
-    op = Create.(valid: true)
-
-  end
-
-  # invalid.
-  it do
-    assert_raises Trailblazer::NotAuthorizedError do
-      op = Create.(valid: false)
-    end
-  end
-
-
-  describe "inheritance" do
-    class Update < Create
-      policy do |params|
-        params[:valid] == "correct"
-      end
-    end
-
-    class Delete < Create
-    end
-
-    it do
-      Create.(valid: true).wont_equal nil
-      Delete.(valid: true).wont_equal nil
-      Update.(valid: "correct").wont_equal nil
-    end
-  end
-
-
-  describe "no policy defined, but included" do
-    class Show < Trailblazer::Operation
-      include Policy
-
-      def process(*)
-      end
-    end
-
-    it { Show.({}).wont_equal nil }
-  end
-end
-
-
-class OpBuilderDenyTest < MiniTest::Spec
-  Song = Struct.new(:name)
-
-  class Create < Trailblazer::Operation
-    include Deny
-
-    builds do |params|
-      deny! unless params[:valid]
-    end
-
-    def process(params)
-    end
-  end
-
-  class Update < Create
-    builds -> (params) do
-      deny! unless params[:valid]
-    end
-  end
-
-  # valid.
-  it do
-    op = Create.(valid: true)
-  end
-
-  # invalid.
-  it do
-    assert_raises Trailblazer::NotAuthorizedError do
-      op = Create.(valid: false)
-    end
-  end
-end
-
-
-
 class OpPunditPolicyTest < MiniTest::Spec
   Song = Struct.new(:name)
   User = Struct.new(:name)
@@ -120,7 +22,7 @@ class OpPunditPolicyTest < MiniTest::Spec
   end
 
   class BlaOperation < Trailblazer::Operation
-    include Policy::Pundit
+    include Policy
     policy BlaPolicy, :create?
 
     def model!(*)
@@ -141,7 +43,7 @@ class OpPunditPolicyTest < MiniTest::Spec
 
   # invalid.
   it do
-    assert_raises Pundit::NotAuthorizedError do
+    assert_raises Trailblazer::NotAuthorizedError do
       op = BlaOperation.({current_user: nil})
     end
   end
@@ -149,7 +51,7 @@ class OpPunditPolicyTest < MiniTest::Spec
 
   # no policy set
   class NoPolicyOperation < Trailblazer::Operation
-    include Policy::Pundit
+    include Policy
     # no policy.
 
     def process(*)
