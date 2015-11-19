60e57db4ad1c140cd0906591d833baf5444455f2
Nick Sutterer <apotonick@gmail.com>
2015-11-19 21:01:08 +1100

    instead of using inheritable_attr for callbacks in an operation, use Heritage.

--- lib/trailblazer/operation/dispatch.rb
+++ lib/trailblazer/operation/dispatch.rb
@@ -3,8 +3,9 @@ require "disposable/callback"
 module Trailblazer::Operation::Dispatch
   def self.included(base)
     base.extend ClassMethods
-    base.inheritable_attr :callbacks
-    base.callbacks = Representable::Cloneable::Hash.new
+
+    base.extend Declarative::Heritage::Inherited
+    base.extend Declarative::Heritage::DSL
   end
 
   def dispatch!(name=:default)
@@ -19,10 +20,21 @@ module Trailblazer::Operation::Dispatch
   end
 
   module ClassMethods
+    def callbacks
+      @callbacks ||= {}
+    end
+
     def callback(name=:default, constant=nil, &block)
       return callbacks[name] unless constant or block_given?
 
-      callbacks[name] ||= Class.new(constant || Disposable::Callback::Group).extend(Representable::Cloneable) # FIXME: why Representable?
+      add_callback(name, constant, &block)
+    end
+
+  private
+    def add_callback(name, constant, &block)
+      heritage.record(:add_callback, name, constant, &block)
+
+      callbacks[name] ||= Class.new(constant || Disposable::Callback::Group)
       callbacks[name].class_eval(&block) if block_given?
     end
   end
