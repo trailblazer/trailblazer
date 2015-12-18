1b3e8a240e86b0a09a58693e9a28604382dca92f
Nick Sutterer <apotonick@gmail.com>
2015-12-19 10:11:51 +1100

    add Callback to autoloading.rb.

--- lib/trailblazer/autoloading.rb
+++ lib/trailblazer/autoloading.rb
@@ -6,7 +6,8 @@ Trailblazer::Operation.class_eval do
   autoload :Controller, "trailblazer/operation/controller"
   autoload :Model,      "trailblazer/operation/model"
   autoload :Collection, "trailblazer/operation/collection"
-  autoload :Dispatch,   "trailblazer/operation/dispatch"
+  autoload :Dispatch,   "trailblazer/operation/dispatch" # TODO: remove in 1.2.
+  autoload :Callback,   "trailblazer/operation/callback"
   autoload :Module,     "trailblazer/operation/module"
   autoload :Representer,"trailblazer/operation/representer"
   autoload :Policy,     "trailblazer/operation/policy"
