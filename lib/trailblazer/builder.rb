7b9be5b823590ae9c4fd95096ed5722549861b85
Nick Sutterer <apotonick@gmail.com>
2016-10-16 10:53:57 +1100

    make Builder a class.

--- lib/trailblazer/builder.rb
+++ lib/trailblazer/builder.rb
@@ -1,17 +1,12 @@
 require "uber/builder"
 
 module Trailblazer
-  module Builder
-    def self.included(includer)
-      includer.send :include, Uber::Builder
-      includer.extend Call
-    end
+  class Builder
+    include Uber::Builder
 
-    module Call
-      def call(*args)
-        # first, call the builders. then, call on the result, the Operation instance.
-        class_builder(self).(*args).(*args)
-      end
+    def self.call(*args)
+      # first, call the builders. then, call on the result, the Operation instance.
+      class_builder(self).(*args).(*args)
     end
   end
 end
