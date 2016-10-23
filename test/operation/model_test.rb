93b3951dca3140ebde793c08a0770355a06c060d
Nick Sutterer <apotonick@gmail.com>
2016-10-23 20:52:30 +1100

    test "external" model. this is simply just a changed pipeline now. it's fucking awesome! <3

--- test/operation/model_test.rb
+++ test/operation/model_test.rb
@@ -2,7 +2,9 @@ require "test_helper"
 require "trailblazer/operation/model"
 
 class ModelTest < Minitest::Spec
-  Song = Struct.new(:title)
+  Song = Struct.new(:id) do
+    def self.find(id); new(id) end
+  end
 
   class Create < Trailblazer::Operation
     include Pipetree
@@ -20,9 +22,25 @@ class ModelTest < Minitest::Spec
   end
 
   # :create new.
-  it { Create.({})["model"].inspect.must_equal %{#<struct ModelTest::Song title=nil>} }
+  it { Create.({})["model"].inspect.must_equal %{#<struct ModelTest::Song id=nil>} }
 
   # TODO: add all the other tests from compat/model_test.rb.
 
   # TODO: override #model! etc. and add params?
+
+  #---
+  # creating the model before operation instantiation (ex Model::External)
+  class Show < Create
+    include Pipetree
+    include Model
+    model Song, :update
+
+    self["pipetree"] = ::Pipetree[
+      Trailblazer::Operation::Model::Build,
+      Trailblazer::Operation::Model::Assign,
+      Trailblazer::Operation::Build,
+    ]
+  end
+
+  it { Show.({id: 1})["model"].inspect.must_equal %{#<struct ModelTest::Song id=1>} }
 end
