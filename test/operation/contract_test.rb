3b599b82afeeba965acf8afa800f7b0051780271
Nick Sutterer <apotonick@gmail.com>
2016-10-25 09:37:49 +1100

    contract tests merged.

--- test/operation/contract_test.rb
+++ test/operation/contract_test.rb
@@ -48,7 +48,7 @@ class ContractTest < Minitest::Spec
   end
 
 
-  # contract(model).validate
+  # contract(model, [admin: true]).validate
   class Create < Trailblazer::Operation
     include Contract
 
@@ -84,6 +84,54 @@ class ContractTest < Minitest::Spec
   it { Update.().must_equal "ContractTest::Form: Object {}" }
   # injected contract overrides class.
   it { Update.({}, "contract.default.class" => Injected = Class.new(Form)).must_equal "ContractTest::Injected: Object {}" }
+
+  # passing Constant into #contract
+  # contract(Object.new, { title: "Bad Feeling" }, Contract)
+  class Operation < Trailblazer::Operation
+    include Contract
+
+    class Contract < Reform::Form
+      property :title, virtual: true
+    end
+
+    def process(params)
+      contract(Object.new, { title: "Bad Feeling" }, Contract)
+
+      validate(params)
+    end
+  end
+
+  # allow using #contract to inject model, options and class.
+  it do
+    contract = Operation.(id: 1)["contract"]
+    contract.title.must_equal "Bad Feeling"
+    contract.must_be_instance_of Operation::Contract
+  end
+
+  # allow using #contract before #validate.
+  class Upsert < Trailblazer::Operation
+    include Contract
+
+    contract do
+      property :id
+      property :title
+      property :length
+    end
+
+    attr_reader :model # FIXME: this sucks!
+    def process(params)
+      @model = Struct.new(:id, :title, :length).new
+      contract.id = 1
+      validate(params) { contract.length = 3 }
+    end
+  end
+
+  it do
+    contract = Upsert.(title: "Beethoven")["contract"]
+    contract.id.must_equal 1
+    contract.title.must_equal "Beethoven"
+    contract.length.must_equal 3
+  end
 end
 
 class ValidateTest < Minitest::Spec
