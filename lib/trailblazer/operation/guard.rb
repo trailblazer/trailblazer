6091213cc22981629a69de71744d1c402cf968aa
Nick Sutterer <apotonick@gmail.com>
2016-10-25 20:55:54 +1100

    add Guard.

---
+++ lib/trailblazer/operation/guard.rb
@@ -0,0 +1,20 @@
+require "trailblazer/operation/policy" # FIXME.
+
+module Trailblazer::Operation::Policy
+  module Guard
+    def self.included(includer)
+      includer.extend DSL # ::policy
+      includer.extend BuildPermission
+      includer.| Evaluate, before: Trailblazer::Operation::Call
+    end
+
+    module BuildPermission
+      # Simply return a proc that, when called, evaluates the Uber:::Value.
+      def build_permission(callable, &block)
+        value = Uber::Options::Value.new(callable || block)
+        # call'ing the Uber value will run either proc or block.
+        ->(*args) { { valid: value.(*args) } }
+      end
+    end
+  end
+end
--- test/operation/guard_test.rb
+++ test/operation/guard_test.rb
@@ -1,24 +1,5 @@
 require "test_helper"
-
-require "trailblazer/operation/policy" # FIXME.
-module Trailblazer::Operation::Policy
-  module Guard
-    def self.included(includer)
-      includer.extend DSL # ::policy
-      includer.extend BuildPermission
-      includer.| Evaluate, before: Trailblazer::Operation::Call
-    end
-
-    module BuildPermission
-      # Simply return a proc that, when called, evaluates the Uber:::Value.
-      def build_permission(callable, &block)
-        value = Uber::Options::Value.new(callable || block)
-        # call'ing the Uber value will run either proc or block.
-        ->(*args) { { valid: value.(*args) } }
-      end
-    end
-  end
-end
+require "trailblazer/operation/guard"
 
 class GuardTest < Minitest::Spec
   #---
