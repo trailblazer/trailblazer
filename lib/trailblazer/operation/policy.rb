3a69381fabcd3abeed7156ac3b707bb307a941bb
Nick Sutterer <apotonick@gmail.com>
2016-10-26 18:10:08 +1100

    make Policy easier to extend. use "valid" now as key.

--- lib/trailblazer/operation/policy.rb
+++ lib/trailblazer/operation/policy.rb
@@ -19,24 +19,29 @@ class Trailblazer::Operation
     end
 
     # This can be subclassed for other policy strategies, e.g. non-pundit Authsome.
+    # NOTE: using a class here is faster than a simple proc: https://twitter.com/apotonick/status/791162989692891136
     class Permission
       def initialize(policy_class, action)
         @policy_class, @action = policy_class, action
       end
 
+      # Instantiate the actual policy object, and call it.
       def call(skills)
-        policy = build_policy(skills) # here, this translates to Pundit interface.
-
-        if policy.send(@action)
-          return { "policy" => policy, valid: true }
-        else
-          return { "policy" => policy, valid: false, "message" => "Breach" }
-        end
+        policy = build_policy(skills)          # this translates to Pundit interface.
+        result!(policy.send(@action), policy)
       end
 
     private
       def build_policy(skills)
         @policy_class.new(skills["user.current"], skills["model"])
+      end
+
+      def result!(result, policy)
+        if result
+          return { "policy" => policy, "valid" => true }
+        else
+          return { "policy" => policy, "valid" => false, "message" => "Breach" }
+        end
       end
     end
   end
@@ -51,7 +56,10 @@ class Trailblazer::Operation
     options[:skills]["policy.result"] = result
 
     # flow control
-    return ::Pipetree::Stop unless result[:valid]
+    return ::Pipetree::Stop unless result["valid"]
     input
   }
 end
+
+# TODO: how could we add something like "log breach"?
+# how to have more than one policy, and then also replace its interpreter with another ? self.| MyInterpreter, replace: "Policy::Evaluator.after_validate"
