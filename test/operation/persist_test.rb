b56d2b58ad75b66db636247efcf1b1ba1f819072
Nick Sutterer <apotonick@gmail.com>
2016-11-07 00:13:01 +1100

    more tests for persist. this is where we can add transactions, rescue and whatnot.

--- test/operation/persist_test.rb
+++ test/operation/persist_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PersistTest < Minitest::Spec
   Song = Struct.new(:title, :saved) do
-    def save; self.saved = true; end
+    def save; title=="Fail!" ? false : self.saved = true; end
   end
 
   class Create < Trailblazer::Operation
@@ -16,8 +16,17 @@ class PersistTest < Minitest::Spec
     self.| Contract::Validate[]
     self.< ->(input, options) { options["1. fail"] = "Validate" }, before: "operation.result"
     self.| Persist[]
+    self.< ->(input, options) { options["2. fail"] = "Persist" }, before: "operation.result"
   end
 
   it { Create.(title: "In Recital")["model"].title.must_equal "In Recital" }
   it { Create.(title: "In Recital")["model"].saved.must_equal true }
+  # failure
+  it do
+    result = Create.(title: "Fail!")
+    result["model"].saved.must_equal nil
+    result["model"].title.must_equal "Fail!"
+    result["2. fail"].must_equal "Persist"
+    result.success?.must_equal false
+  end
 end
