0b3cb05705524c530d4fdabc930096381303c84b
Nick Sutterer <apotonick@gmail.com>
2016-11-19 19:37:49 +0100

    use class methods in Contract, too as it's a thousand times more readable.

--- lib/trailblazer/operation/validate.rb
+++ lib/trailblazer/operation/validate.rb
@@ -6,18 +6,23 @@ module Trailblazer::Operation::Contract
   module Validate
     extend Trailblazer::Operation::Stepable
 
-    def self.import!(operation, import, key: nil, name: "default")
-      import.(:&, ->(input, options) { options["params.validate"] = key ? options["params"][key] : options["params"] }, # FIXME: introduce nested pipes and pass composed input instead.
+    def self.import!(operation, import, **args)
+      import.(:&, ->(input, options) { extract_params!(input, options, **args) },
         name: "validate.params.extract")
 
       # call the actual contract.validate(params)
-      import.(:&, ->(operation, options) {
-
-        operation.validate(options["params.validate"], contract: operation["contract.#{name}"]) }, # FIXME: how could we deal here with polymorphic keys?
-
+      import.(:&, ->(operation, options) { validate!(operation, options, **args) },
         name: "contract.validate")
+      end
 
-      operation.send :include, self
+    def self.extract_params!(operation, options, key:nil, **)
+      # FIXME: introduce nested pipes and pass composed input instead.
+      options["params.validate"] = key ? options["params"][key] : options["params"]
+    end
+
+    def self.validate!(operation, options, name:"default", path:"contract", **)
+      operation["result.#{path}"] = result = operation["contract.#{name}"].(options["params.validate"]) # FIXME: how could we deal here with polymorphic keys?
+      result.success?
     end
 
     def validate(params, contract:self["contract.default"], path:"contract") # :params
