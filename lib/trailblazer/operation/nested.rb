c9f0e7f13f382c8711aac01bb632da724656d579
Nick Sutterer <apotonick@gmail.com>
2016-11-29 09:05:35 +0100

    no need to go via the operation when we have the skills.

--- lib/trailblazer/operation/nested.rb
+++ lib/trailblazer/operation/nested.rb
@@ -1,11 +1,14 @@
 module Trailblazer::Operation::Nested
   extend Trailblazer::Operation::Macro
 
+  # Please note that the instance_variable_get are here on purpose since I don't
+  # want to introduce readers that are used once.
+
   def self.import!(operation, import, step)
     import.(:>, ->(input, options) {
-      result = step._call(*input.instance_variable_get(:@skills).to_runtime_data)
+      result = step._call(*options.to_runtime_data)
       result.instance_variable_get(:@data).to_mutable_data.each do |k,w|
-        operation[k]=w
+        operation[k] = w
       end
     }, {} )
   end
