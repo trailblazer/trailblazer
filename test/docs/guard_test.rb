d4965ecbd202475ca70d668aa2eff8732ae48ebc
Nick Sutterer <apotonick@gmail.com>
2016-11-29 21:04:54 +0100

    more doc tests for guard.

--- test/docs/guard_test.rb
+++ test/docs/guard_test.rb
@@ -1,17 +1,18 @@
 require "test_helper"
 
 class DocsGuardProcTest < Minitest::Spec
-
   #--
   # with proc
   #:proc
   class Create < Trailblazer::Operation
     self.| Policy::Guard[ ->(options) { options["params"][:pass] } ]
     self.| :process
+    #~pipeonly
 
     def process(*)
       self["x"] = true
     end
+    #~pipeonly end
   end
   #:proc end
 
@@ -24,12 +25,40 @@ class DocsGuardProcTest < Minitest::Spec
 
 
 
+
    #---
   #- Guard inheritance
   class New < Create
   end
 
   it { New["pipetree"].inspect.must_equal %{[>>operation.new,&policy.guard.evaluate,>process]} }
+end
+
+class DocsGuardTest < Minitest::Spec
+  #---
+  # with Callable
+  #:callable
+  class MyGuard
+    include Uber::Callable
+
+    def call(options)
+      options["params"][:pass]
+    end
+  end
+  #:callable end
+
+  #:callable-op
+  class Create < Trailblazer::Operation
+    self.| Policy::Guard[ MyGuard.new ]
+    self.| :process
+    #~pipe-only
+    def process(*); self[:x] = true; end
+    #~pipe-only end
+  end
+  #:callable-op end
+
+  it { Create.(pass: false)[:x].must_equal nil }
+  it { Create.(pass: true)[:x].must_equal true }
 end
 
 class DocsGuardClassLevelTest < Minitest::Spec
@@ -39,7 +68,9 @@ class DocsGuardClassLevelTest < Minitest::Spec
   class Create < Trailblazer::Operation
     self.| Policy::Guard[ ->(options) { options["current_user"] == Module } ],
       before: "operation.new"
+    #~pipe--only
     self.| ->(options) { options["x"] = true }
+    #~pipe--only end
   end
   #:class-level end
 
--- test/operation/guard_test.rb
+++ test/operation/guard_test.rb
@@ -1,26 +1,6 @@
 require "test_helper"
 
-class GuardTest < Minitest::Spec
 
-
-  # with Callable, operation passed in.
-  class Update < Trailblazer::Operation
-    class MyGuard
-      include Uber::Callable
-      def call(options); options["params"][:pass] end
-    end
-
-    self.| Policy::Guard[ MyGuard.new ]
-    self.| :process
-
-    def process(*); self[:x] = true; end
-  end
-
-  it { Update.(pass: false)[:x].must_equal nil }
-  it { Update.(pass: true)[:x].must_equal true }
-
-
-end
 
 
 # FIXME: what about block passed to ::policy?
