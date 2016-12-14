75af1a17953c15b19fe678c6025a5fa5d4cdba7d
Nick Sutterer <apotonick@gmail.com>
2016-12-14 20:42:18 +0200

    preparing 2.0 release.

--- draft-1.2.rb
+++
@@ -1,7 +0,0 @@
-representer :deserializer, Representer::Create (do)
-  or
-Op.(representer.deserializer.class: Representer::Create)
-
-
-representer!(name)
-  @options[representer.deserializer.class] || self.class[representer.deserializer.class]
