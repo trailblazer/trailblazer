0ec658666f3f6352868571d0e5a57f83b9c1112d
Nick Sutterer <apotonick@gmail.com>
2017-01-18 09:27:47 +1100

    introduce Persist().

--- lib/trailblazer/operation/persist.rb
+++ lib/trailblazer/operation/persist.rb
@@ -1,14 +1,10 @@
 class Trailblazer::Operation
   module Contract
-    module Persist
-      def self.import!(operation, import, method: :save, name: "default")
-        path = "contract.#{name}"
+    def self.Persist(method: :save, name: "default")
+      path = "contract.#{name}"
+      step = ->(input, options) { options[path].send(method) }
 
-        import.(:&, ->(input, options) { options[path].send(method) }, # TODO: test me.
-          name: "persist.save")
-      end
+      [ step, name: "persist.save" ]
     end
   end
-
-  DSL.macro!(:Persist, Contract::Persist, Contract.singleton_class) # Contract::Persist()
 end
