d64500ca80a20d2414f76a5efe8a521a2124403d
Nick Sutterer <apotonick@gmail.com>
2017-01-19 11:49:43 +1100

    more invocation tests.

--- test/docs/operation_test.rb
+++ test/docs/operation_test.rb
@@ -4,6 +4,32 @@ class DocsOperationExampleTest < Minitest::Spec
   Song = Struct.new(:id, :title, :created_by) do
     def save; true; end
   end
+  User = Struct.new(:name)
+
+  #:invocation-dep
+  class Create < Trailblazer::Operation
+    step     Model( Song, :new )
+    step     :assign_current_user!
+    # ..
+    def assign_current_user!(options)
+      options["model"].created_by = options["current_user"]
+    end
+  end
+  #:invocation-dep end
+
+  it do
+    current_user = User.new("Ema")
+  #:invocation-dep-call
+  result = Create.( { title: "Roxanne" }, "current_user" => current_user )
+  #:invocation-dep-call end
+
+  #:invocation-dep-res
+  result["current_user"] #=> #<User name="Ema">
+  result["model"]        #=> #<Song id=nil, title=nil, created_by=#<User name="Ema">>
+  #:invocation-dep-res end
+  end
+
+  it { Create.({ title: "Roxanne" }, "current_user" => Module).inspect("model").must_equal %{<Result:true [#<struct DocsOperationExampleTest::Song id=nil, title=nil, created_by=Module>] >} }
 
   #:op
   class Song::Create < Trailblazer::Operation
