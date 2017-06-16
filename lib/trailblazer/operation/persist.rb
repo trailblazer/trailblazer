74d436abd236b5adabdba9a98e492bc82230d05a
Nick Sutterer <apotonick@gmail.com>
2017-06-16 12:50:36 +0200

    use new API in Persist.

--- lib/trailblazer/operation/persist.rb
+++ lib/trailblazer/operation/persist.rb
@@ -2,9 +2,11 @@ class Trailblazer::Operation
   module Contract
     def self.Persist(method: :save, name: "default")
       path = "contract.#{name}"
-      step = ->(input, options) { options[path].send(method) }
+      step = ->(options, **) { options[path].send(method) }
 
-      [ step, name: "persist.save" ]
+      task = Railway::TaskBuilder.( step )
+
+      [ task, { name: "persist.save" }, {} ]
     end
   end
 end
