5479ba86697d062e81c7ff7561727eda3168b556
Abdelkader Boudih <terminale@gmail.com>
2018-05-03 22:59:46 +0100

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,12 @@ require "delegate"
 require "trailblazer"
 require "minitest/autorun"
 
+# TODO: convert tests to non-rails.	
+require "reform"	
+require "reform/form/active_model/validations"	
+Reform::Form.class_eval do	
+  include Reform::Form::ActiveModel::Validations	
+end
 # require "trailblazer/deprecation/context.rb"
 
 module Mock
