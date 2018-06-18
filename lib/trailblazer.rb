1fcc50bf822b5b02cbcafb28c518bee278e0e6bb
Nick Sutterer <apotonick@gmail.com>
2018-06-18 08:54:07 +0200

    Remove `task` and `input_output`, those are implemented in `activity`.

--- lib/trailblazer.rb
+++ lib/trailblazer.rb
@@ -3,11 +3,7 @@ require "trailblazer/version"
 require "trailblazer/operation"
 require "trailblazer/dsl"
 
-require "trailblazer/task"
-
 require "trailblazer/operation/deprecations"
-require "trailblazer/operation/inject"
-require "trailblazer/operation/input_output"
 
 require "trailblazer/macro"
 require "trailblazer/macro/contract"
