a286c1557352ea26ed8b310b8c2a6dd0c59701d4
Nick Sutterer <apotonick@gmail.com>
2023-06-14 12:32:45 +0200

    clean up test_helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,38 +1,3 @@
-require "pp"
-require "delegate"
-require "trailblazer"
 require "minitest/autorun"
 
-# TODO: convert tests to non-rails.
-require "reform"
-require "reform/form/active_model/validations"
-Reform::Form.class_eval do
-  include Reform::Form::ActiveModel::Validations
-end
-# require "trailblazer/deprecation/context.rb"
-
-module Mock
-  class Result
-    def initialize(bool); @bool = bool end
-    def success?; @bool end
-    def errors; ["hihi"] end
-  end
-end
-
-module Test
-  module ReturnCall
-    def self.included(includer)
-      includer._insert :_insert, ReturnResult, {replace: Trailblazer::Operation::Result::Build}, ReturnResult, ""
-    end
-  end
-  ReturnResult = ->(last, input, options) { input }
-end
-
-Minitest::Spec::Operation = Trailblazer::Operation
-
-Memo = Struct.new(:id, :body) do
-  def self.find(id)
-    return new(id, "Yo!") if id
-    nil
-  end
-end
+require "trailblazer"
