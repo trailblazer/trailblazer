4af7cebe6178e37d974938854f29582ef3d0c7ed
Nick Sutterer <apotonick@gmail.com>
2023-06-14 17:54:52 +0200

    finally, after a decade, add tests again, for this gem!

---
+++ test/trailblazer_test.rb
@@ -0,0 +1,69 @@
+require "test_helper"
+require "reform/form/dry"
+require "trailblazer/macro"
+require "trailblazer/macro/contract"
+
+class TrailblazerTest < Minitest::Spec
+  Song = Struct.new(:title)
+
+  class Create < Trailblazer::Operation
+    class Form < Reform::Form
+      include Reform::Form::Dry
+      property :title
+
+      validation do
+        params do
+          required(:title).filled(min_size?: 2)
+        end
+      end
+    end
+
+    step Model(Song, :new)
+    step Contract::Build(constant: Form)
+    step Contract::Validate()
+    step Contract::Persist(method: :sync)
+  end
+
+  it do
+    result = nil
+
+    output, _ = capture_io do
+      result = Create.wtf?(params: {title: "Dead and Gone"})
+    end
+
+    assert_equal output, %(TrailblazerTest::Create
+|-- \e[32mStart.default\e[0m
+|-- \e[32mmodel.build\e[0m
+|-- \e[32mcontract.build\e[0m
+|-- contract.default.validate
+|   |-- \e[32mStart.default\e[0m
+|   |-- \e[32mcontract.default.params_extract\e[0m
+|   |-- \e[32mcontract.default.call\e[0m
+|   `-- End.success
+|-- \e[32mpersist.save\e[0m
+`-- End.success
+)
+    assert_equal result.success?, true
+    assert_equal result[:model].to_h, {:title=>"Dead and Gone"}
+
+  # invalid!
+    output, _ = capture_io do
+      result = Create.wtf?(params: {})
+    end
+
+    assert_equal output, %(TrailblazerTest::Create
+|-- \e[32mStart.default\e[0m
+|-- \e[32mmodel.build\e[0m
+|-- \e[32mcontract.build\e[0m
+|-- contract.default.validate
+|   |-- \e[32mStart.default\e[0m
+|   |-- \e[32mcontract.default.params_extract\e[0m
+|   |-- \e[33mcontract.default.call\e[0m
+|   `-- End.failure
+`-- End.failure
+)
+
+    assert_equal result.success?, false
+    assert_equal result[:mode].to_h, {}
+  end
+end
--- trailblazer.gemspec
+++ trailblazer.gemspec
@@ -35,6 +35,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake"
   spec.add_development_dependency "minitest"
   spec.add_development_dependency "minitest-line"
+  spec.add_development_dependency "dry-validation"
 
   spec.required_ruby_version = '>= 2.5.0'
 end
